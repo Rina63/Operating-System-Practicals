df71295a55d29513a58ab94b51f3f0d481d45846
Rina63 <ireneobiekwe63@gmail.com>
2026-06-26 05:33:53 +0100

    Update without_mutex.c

--- Practical1_Mutex/without_mutex.c
+++ Practical1_Mutex/without_mutex.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include <pthread.h>
 
+int counter = 0;
+
+void* increment(void* arg) {
+
+    for (int i = 0; i < 100000; i++) {
+        counter++;
+    }
+
+    return NULL;
+}
+
+int main() {
+
+    pthread_t t1, t2, t3, t4, t5;
+
+    pthread_create(&t1, NULL, increment, NULL);
+    pthread_create(&t2, NULL, increment, NULL);
+    pthread_create(&t3, NULL, increment, NULL);
+    pthread_create(&t4, NULL, increment, NULL);
+    pthread_create(&t5, NULL, increment, NULL);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+    pthread_join(t3, NULL);
+    pthread_join(t4, NULL);
+    pthread_join(t5, NULL);
+
+    printf("Final Counter = %d\n", counter);
+
+    return 0;
+}
